dddb012b4070138e37b33cb9430efad02d4be00d
gadepall <gadepall@gmail.com>
2023-05-23 13:11:15 +0530

    Add existing files

--- fm/codes/msg/input.py
+++ fm/codes/msg/input.py
@@ -6,15 +6,17 @@ import matplotlib.pyplot as plt
 
 
 #Fs, audio_data = wavfile.read("Sound.wav")
-sample_rate, audio_data = wavfile.read("Sound.wav")
+#Reading the audio data into the variable audio_data
+sample_rate, audio_data = wavfile.read("fm/codes/msg/Sound_Noise.wav")
 
 #perform fft on audio signal
-i = np.fft.fft(audio_data)
+X_i = np.fft.fft(audio_data)
 
 #calculate the frequency range
 f_i = np.fft.fftfreq(len(audio_data), d=1/sample_rate)
+print(f_i)
 
-plt.plot(f_i, np.abs(i))
+plt.plot(f_i, np.abs(X_i))
 plt.xlabel('Frequency (Hz)')
 plt.ylabel('Magnitude')
 plt.title('Input Signal')
