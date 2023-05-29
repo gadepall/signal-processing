e96a2438e1a4c53f3aef4de6f8b02b2b41fd70a3
Siva Krishna <107164618+siva-krishna-IITH-2022@users.noreply.github.com>
2023-05-29 10:32:54 +0530

    Add files via upload

--- fm/codes/msg/msg_bw.py
+++ fm/codes/msg/msg_bw.py
@@ -1,30 +1,28 @@
+'''==========================================================
+Code by G V V Sharma
+May 27, 2023,
+Released under GNU/GPL
+https://www.gnu.org/licenses/gpl-3.0.en.html
+=========================================================='''
+ 
 #Calculating Bandwith of the message signal
 
-from scipy.fftpack import fft,fftshift
+from scipy.fftpack import fft
 import numpy as np
 from scipy.io import wavfile
-import scipy.signal as signal
 import matplotlib.pyplot as plt
 
-
-#Fs, audio_data = wavfile.read("Sound.wav")
-#Reading the audio data into the variable audio_data
 sample_rate, audio_data = wavfile.read("fm/codes/msg/Sound_Noise.wav")
 
-#perform fft on audio signal
 X_i = np.fft.fft(audio_data)
-
-# Calculate power spectral density
 psd = np.abs(X_i)**2
-
-# Calculate frequency range
 freqs = np.fft.fftfreq(len(psd), 1/sample_rate)
 
 # Find frequency range with significant power
 mask = psd > 0.1*np.max(psd)
 freq_range = freqs[mask]
 bandwidth = max(freq_range) - min(freq_range)
-print("fmax",max(freq_range))
-print('fmin',min(freq_range))
-print('Bandwidth:', bandwidth,'Hz')
+#print("fmax",max(freq_range))
+#print('fmin',min(freq_range))
+print('Bandwidth of msg signal:', bandwidth,'Hz')
 
--- fm/codes/msg/msg_spec_amp.py
+++ fm/codes/msg/msg_spec_amp.py
@@ -1,26 +1,25 @@
-from scipy.fftpack import fft,fftshift
+'''==========================================================
+Code by G V V Sharma
+May 27, 2023,
+Released under GNU/GPL
+https://www.gnu.org/licenses/gpl-3.0.en.html
+=========================================================='''
+ 
+#Spectrum of message signal using builtin FFT algorithm of the python library ’Numpy'
+ 
+from scipy.fftpack import fft
 import numpy as np
 from scipy.io import wavfile
-import scipy.signal as signal
 import matplotlib.pyplot as plt
 
-
-#Fs, audio_data = wavfile.read("Sound.wav")
-#Reading the audio data into the variable audio_data
 sample_rate, audio_data = wavfile.read("fm/codes/msg/Sound_Noise.wav")
-print(sample_rate)
-#perform fft on audio signal
 X_i = np.fft.fft(audio_data)
-
-#calculate the frequency range
 f_i = np.fft.fftfreq(len(audio_data), d=1/sample_rate)
-print(f_i)
 
 plt.plot(f_i, np.abs(X_i))
 plt.xlabel('Frequency (Hz)')
 plt.ylabel('Magnitude')
-plt.title('Input Signal')
-#plt.savefig('inputs.pdf')
+plt.title('Message Signal')
+plt.savefig("fm/msg/figs/msg_spec_amp.pdf")
 plt.show()
-
 
