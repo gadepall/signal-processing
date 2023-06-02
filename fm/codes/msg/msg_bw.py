fd03f9e1ca39a9f8c3b03fef89bb32e57cdba4ee
Siva Krishna <107164618+siva-krishna-IITH-2022@users.noreply.github.com>
2023-06-02 08:29:27 +0530

    Add files via upload

--- fm/codes/msg/msg_bw.py
+++ fm/codes/msg/msg_bw.py
@@ -12,17 +12,21 @@ import numpy as np
 from scipy.io import wavfile
 import matplotlib.pyplot as plt
 
-sample_rate, audio_data = wavfile.read("fm/codes/msg/Sound_Noise.wav")
+sample_rate, audio_data = wavfile.read("fm/input-audio/Sound.wav")
 
 X_i = np.fft.fft(audio_data)
 psd = np.abs(X_i)**2
 freqs = np.fft.fftfreq(len(psd), 1/sample_rate)
 
 # Find frequency range with significant power
-mask = psd > 0.1*np.max(psd)
+threshold = 0.1*np.max(psd)
+mask = psd > threshold
+
 freq_range = freqs[mask]
-bandwidth = max(freq_range) - min(freq_range)
-#print("fmax",max(freq_range))
-#print('fmin',min(freq_range))
+fmax = max(freq_range)
+fmin = min(freq_range)
+print("f_max is:",fmax)
+print("f_min is:",fmin)
+bandwidth = fmax - fmin
 print('Bandwidth of msg signal:', bandwidth,'Hz')
 
